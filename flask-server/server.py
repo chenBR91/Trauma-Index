9df4c10a6dfa40b791100b8affd8db5b657f8ddb
AlexBrown13 <rupcgroup25.6@gmail.com>
2025-11-14 14:51:34 +0200

    added mongodb

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
+
+
+# Set up MongoDB connection
+client = MongoClient('mongodb://localhost:27017/Demo.students')
+db = client['students'] # database name
+collection = db['Demo'] # mongodb://localhost:27017
+
 
 # Routes
 
