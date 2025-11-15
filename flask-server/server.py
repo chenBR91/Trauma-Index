1e055f34a0329f24bb28c80d2df17c740f3eaca9
AlexBrown13 <rupcgroup25.6@gmail.com>
2025-11-15 09:55:46 +0200

    fetch and load user data from external API

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+
 from user.models import User
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Prevent the cors error
 
 
 # Set up MongoDB connection
@@ -12,6 +13,7 @@ try:
     client = MongoClient('localhost', 27017)  # mongodb://localhost:27017
     db = client['Demo'] # this is the database
     collection = db['students'] # this is the collection
+    collectionUser = db['user']
 except:
     print('Error - Cannot connect to db')
 
@@ -26,19 +28,68 @@ def signup():
 @app.route("/add_user", methods=['POST']) 
 def add_user():
     collection.insert_one({'name': 'Alex', 'age': 30, 'phone': '8888', 'email': 'email'})
-    all_studets = collection.find()
     return jsonify({'users': 'fd'})
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['POST', 'GET'])
 def get_all_users():
-    all_users = collection.find()
-    print(f'users {all_users}')
-    return jsonify({'users': 'fd'})
+    if request.method == 'GET':
+        allUsers = collection.find()
+        usersJson = []
+        for user in allUsers:
+            name = user['name']
+            age = user['age']
+            phone = user['phone']
+            email = user['email']
+            id = user['_id']
+
+            userDict = {
+                "id": str(id),
+                "name": str(name),
+                "age": int(age),
+                "phone": str(phone),
+                "email": str(email)
+            }
+
+            usersJson.append(userDict)
+            
+
+    return jsonify({'users': usersJson})
+
 
 @app.route("/api", methods=['GET'])
 def users():
     return jsonify({"users": ["user1", "user2"]})
+
+
+# API users only once
+import requests
+@app.route("/api/read/data/only-once", methods=['GET'])
+def readData():
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    users = []
+    for user in data:
+        completed = user['completed']
+        id = user['id']
+        title = user['title']
+        userId = user['userId']
+
+        userJson = {
+            'completed': completed,
+            'id': id,
+            'title': title,
+            'userId': userId
+        }
+
+        users.append(userJson)
+
+    collectionUser.insert_many(users)
+    return jsonify({"user": data})
+
 
 
 if __name__ == "__main__":
